eb560c954d30b8ee24dacaaa27d5c5a5d296cbcf
Ashparshp <ashparsh.connects@gmail.com>
2025-01-28 13:39:41 +0530

    Refactor App component by removing unused imports, loading state, and routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,9 @@
-import { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import BlogPage from "./pages/BlogPage";
-import NotFoundPage from "./pages/NotFoundPage";
-import InteractiveGames from "./games/InteractiveGames";
-import ColorMatchGame from "./games/components/ColorMatchGame/index";
-import TypingSpeedTest from "./games/components/TypingSpeedTest/index";
-import FirstLoading from "./components/common/EnhancedLoadingSpinner";
+import React from 'react'
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => setIsLoading(false), 2000);
-  }, []);
-
-  if (isLoading) {
-    return <FirstLoading />;
-  }
-
   return (
-    <>
+    <div>App</div>
+  )
+}
 
-    <h1>
-      Hi
-    </h1>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/games" element={<InteractiveGames />} />
-          <Route path="/games/color-match" element={<ColorMatchGame />} />
-          <Route path="/games/typing-test" element={<TypingSpeedTest />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
-};
-
-export default App;
+export default App
